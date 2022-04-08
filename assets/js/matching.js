292655b6772be5358ccfbff6ce5097845a20cb5e
nicolelelelele <nicolechong.2019@sis.smu.edu.sg>
2022-04-09 05:25:31 +0800

    update api url

--- assets/js/matching.js
+++ assets/js/matching.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 async function getMatchedMigrantWorker(donationID) {
-    let response = await fetch("http://http://ec2-13-250-122-219.ap-southeast-1.compute.amazonaws.com/:5003/matchingAlgorithm/" + donationID)
+    let response = await fetch("http://ec2-13-250-122-219.ap-southeast-1.compute.amazonaws.com:5003/matchingAlgorithm/" + donationID)
     let responseJson = await response.json()
 
     if (responseJson.code == 200) {
